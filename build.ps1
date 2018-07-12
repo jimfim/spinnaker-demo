17928a54185a3d40c977f26fdf1ce990d98ca7c5
unknown <jfurey@mywebgrocer.com>
2018-07-12 23:48:48 +0100

    update

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
-$version = 0.3;
+$version = 0.4;
 
 docker build -t jimfim/spinnaker-demo:$version .
 docker tag  jimfim/spinnaker-demo:$version jimfim/spinnaker-demo:latest
 
-#docker push jimfim/spinnaker-demo:$version
-#docker push jimfim/spinnaker-demo:latest
+docker push jimfim/spinnaker-demo:$version
+docker push jimfim/spinnaker-demo:latest
 
 helm package .\chart\spinnaker-demo\ --destination .\repo\
 helm repo index --url https://github.com/jimfim/spinnaker-demo/repo .\repo\
